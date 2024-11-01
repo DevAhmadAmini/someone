32403005a5df0aec1787714cf9ae566f54cd4374
DevAhmadAmini <devahmadamini@gmail.com>
2024-11-01 13:18:19 +0430

    added new branch and service page

---
+++ lib/service.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Service {
+  final String id;
+  Service({
+    required this.id,
+  });
+
+  void loginUser() {
+    print("User has been logged in");
+  }
+}
